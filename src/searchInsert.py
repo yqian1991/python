5a29f631bb3566f8d0c02c9b251b345a43203f39
qianyu <jinggeqianyu1991@163.com>
2014-10-03 21:09:48 -0400

    add

---
+++ src/searchInsert.py
@@ -0,0 +1,19 @@
+'''
+Created on Oct 1, 2014
+
+@author: yqian33
+'''
+class Solution:
+    # @param A, a list of integers
+    # @param target, an integer to be inserted
+    # @return integer
+    def bsearch(self, A, start, end, target):
+        mid = 
+        if A[]
+    def searchInsert(self, A, target):
+        l = len(A)
+        self.bsearch(0, l-1, target)
+if __name__ == '__main__':
+    sol = Solution()
+    print sol.searchInsert([2,4,5,6,7,9], 1)
+    pass

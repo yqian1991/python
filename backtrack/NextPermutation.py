2831141285180ffd3a7b697e07b7124d1c16f21d
qianyu668899 <jinggeqianyu1991@gmail.com>
2015-08-22 10:10:41 -0400

    Add backtrack problems solutions

--- backtrack/NextPermutation.py
+++ backtrack/NextPermutation.py
@@ -3,23 +3,23 @@ class Solution:
     # @return a list of integer
     def nextPermutation(self, num):
 		n = len(num) - 2
-		while n>=0:
+		while n >= 0:
 			if num[n]<num[n+1]:
 				break
 			else:
 				n -= 1
 		if n == -1:
 			return reverse(num)
-		
-
-		
-		
-		print num[n]
+		else:
+			print n
+			small = min(num[(n+1):])
+			print small
+			small_index = num.index(small)
+			num[n], num[small_index] = num[small_index], num[n]
+			sorted(num[n+1:])
+			return num
 			
 			
-
-
-
 if __name__=="__main__":
 	sol = Solution()
-	sol.nextPermutation([1,4,3,2,1])
+	print sol.nextPermutation([1,4,3,2,1])
---
+++ backtrack/PermutationUnique.py
@@ -0,0 +1,30 @@
+class Solution:
+    # @param num, a list of integer
+    # @return a list of lists of integers
+    def backtrack(self, index, num, n, res):
+        if index == n:
+            add = num[:]
+            res.append(add)
+        else:
+            flag = 0
+            for i in range(index, n+1):
+                for j in range(index, i):
+                    if num[j] == num[i]:
+                        flag = 1
+                        break
+                    flag = 0
+                if flag == 1:
+                    continue
+                num[i], num[index] = num[index], num[i]
+                self.backtrack(index+1, num, n, res)
+                num[i], num[index] = num[index], num[i]
+                
+    def permuteUnique(self, num):
+        num = sorted(num)
+        res=[]
+        self.backtrack(0, num, len(num)-1, res)
+        return res
+
+if __name__== "__main__":
+    sol = Solution()
+    print sol.permuteUnique([1,1,2,2,3 ,4])

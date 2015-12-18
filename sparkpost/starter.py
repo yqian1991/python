dfe62406b100de54d410882dd0cef4f782449318
yqian <jinggeqianyu1991@gmail.com>
2015-12-18 11:46:06 -0500

    First email by sparkpost

---
+++ sparkpost/starter.py
@@ -0,0 +1,13 @@
+from sparkpost import SparkPost
+
+sp = SparkPost("663c9d0ec49f661a63e8becf9fa62b35d8303e50")
+
+response = sp.transmissions.send(
+    recipients=['yuq@surveymonkey.com'],
+    html='<p>Hello world</p>',
+    from_email='test@sparkpostbox.com',
+    subject='Hello from python-sparkpost'
+)
+
+print response
+# outputs {u'total_accepted_recipients': 1, u'id': u'47960765679942446', u'total_rejected_recipients': 0}
